d0fe401dad8460b74a5288f1f2a30a9f0856879d
rodolfoocampo <j.rodolfo.ocampo@gmail.com>
2017-04-22 18:22:02 -0500

    Da máximo puntaje cuando hay 4 in a Row

--- connect-4.lisp
+++ connect-4.lisp
@@ -7,17 +7,21 @@
     (setf dos 0)
     (setf puntos-uno 0)
     (setf puntos-dos 0)
+    (setf TwoInRow 0 OneInRow 0)
     
     (dotimes (i 6)
         (dolist (n estado)
             (case (nth i (car n))
-                (0 (incf unos)(incf dos))
+                (0 (incf unos)(incf dos)(setf ultima-ficha 0))
                 (1 (incf unos)(setf dos 0)(setf ultima-ficha 1))
                 (2 (incf dos)(setf unos 0)(setf ultima-ficha 2)))
-                (print 'unos)
-                (print unos)
-                (print 'dos)
-                (print dos)
+                (if (eq ultima-ficha 2)(incf TwoInRow)(setf TwoInRow 0))
+                (if (eq ultima-ficha 1)(incf OneInRow)(setf OneInRow 0))
+                (if (eq TwoInRow 4)(incf puntos-dos 1000))
+                (if (eq OneInRow 4)(incf puntos-uno 1000))
+                
+                
+               
                 (cond 
                     ((and (> i 0) (eq 0 (nth i (car n))) (eq 0 (nth (- i 1) (car n)))) (setf unos 0) (setf dos 0)))
                 (cond ((eq 4 unos) (incf puntos-uno) (decf unos)))
@@ -26,7 +30,10 @@
         )
         (setf unos 0 dos 0)
     )
-    (print 'ok)
-    (print puntos-uno)
+    (print TwoInRow)
+    (print OneInRow)
     (print puntos-dos)
+    
 )
+
+(check4horizontal board)
