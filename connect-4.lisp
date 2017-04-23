b85d4c4ea2247c879f53251f5544fbf622762905
rodolfoocampo <j.rodolfo.ocampo@gmail.com>
2017-04-22 19:32:00 -0500

    Suma 1000 si hay 4 in a row

--- connect-4.lisp
+++ connect-4.lisp
@@ -1,5 +1,5 @@
-(setq board '(((0 0 0 0 0 0 0 0) 0)((2 0 0 0 0 0 0 0) 1)((2 0 0 0 0 0 0 0) 1)
-((2 0 0 0 0 0 0 0) 1)((2 0 0 0 0 0 0 0) 1)((2 0 0 0 0 0 0 0) 1)) )
+(setq board '(((0 0 0 0 0 0 0 0) 0)((2 2 2 2 0 0 0 0) 1)((2 1 0 0 0 0 0 0) 1)
+((2 0 0 0 0 0 0 0) 1)((2 0 0 0 0 0 0 0) 1)((2 1 0 0 0 0 0 0) 1)) )
 
 (defun check4horizontal (estado)
     (setf ultima-ficha nil)
@@ -15,10 +15,11 @@
                 (0 (incf unos)(incf dos)(setf ultima-ficha 0))
                 (1 (incf unos)(setf dos 0)(setf ultima-ficha 1))
                 (2 (incf dos)(setf unos 0)(setf ultima-ficha 2)))
+                
                 (if (eq ultima-ficha 2)(incf TwoInRow)(setf TwoInRow 0))
                 (if (eq ultima-ficha 1)(incf OneInRow)(setf OneInRow 0))
-                (if (eq TwoInRow 4)(incf puntos-dos 1000))
-                (if (eq OneInRow 4)(incf puntos-uno 1000))
+                (if (eq TwoInRow 4)(incf puntos-dos 100))
+                (if (eq OneInRow 4)(incf puntos-uno 100))
                 
                 
                
@@ -37,7 +38,7 @@
 )
 
 (defun check4vertical (state)
-	(setf ficha-abajo nil)
+	(setf ultima-ficha nil)
 	(setf unos 0 dos 0)
 	(setf i 0)
 	(setf puntaje-uno 0 puntaje-dos 0)
@@ -46,17 +47,24 @@
 		(loop 
 		   (case (nth i (car n))
                 (0 (incf unos)(incf dos)(setf ultima-ficha 0))
-                (1 (incf unos)(setf dos 0))
-                (2 (incf dos)(setf unos 0)))
+                (1 (incf unos)(setf dos 0)(setf ultima-ficha 1))
+                (2 (incf dos)(setf unos 0)(setf ultima-ficha 2)))
+                
+            (if (eq ultima-ficha 2)(incf TwoInRow)(setf TwoInRow 0))
+            (if (eq ultima-ficha 1)(incf OneInRow)(setf OneInRow 0))
+            
             (incf i)
             (when (or (> i 5) (eq 4 unos) (eq dos 4)) (return (setf i 0)))
 		)
+		
 		(if (eq unos 4) (incf puntaje-uno))
 		(if (eq dos 4) (incf puntaje-dos))
+		(if (eq TwoInRow 4)(incf puntaje-dos 1000))
+        (if (eq OneInRow 4)(incf puntaje-uno 1000))
 		(setf unos 0 dos 0)
 	)
 	(print puntaje-uno)
 	(print puntaje-dos)
 )
 
-(check4horizontal board)
+(check4vertical board)
