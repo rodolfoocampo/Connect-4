0b7ffcd2acc21f02ca83feef575d3769f485c627
rodolfoocampo <j.rodolfo.ocampo@gmail.com>
2017-04-22 19:18:20 -0500

    Hice función que calcula el puntaje vertical

--- connect-4.lisp
+++ connect-4.lisp
@@ -36,4 +36,27 @@
     
 )
 
+(defun check4vertical (state)
+	(setf ficha-abajo nil)
+	(setf unos 0 dos 0)
+	(setf i 0)
+	(setf puntaje-uno 0 puntaje-dos 0)
+	
+	(dolist (n state)
+		(loop 
+		   (case (nth i (car n))
+                (0 (incf unos)(incf dos)(setf ultima-ficha 0))
+                (1 (incf unos)(setf dos 0))
+                (2 (incf dos)(setf unos 0)))
+            (incf i)
+            (when (or (> i 5) (eq 4 unos) (eq dos 4)) (return (setf i 0)))
+		)
+		(if (eq unos 4) (incf puntaje-uno))
+		(if (eq dos 4) (incf puntaje-dos))
+		(setf unos 0 dos 0)
+	)
+	(print puntaje-uno)
+	(print puntaje-dos)
+)
+
 (check4horizontal board)
