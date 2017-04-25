dab46d269b268723b166dc2229b82d2d0aad7416
rodolfoocampo <j.rodolfo.ocampo@gmail.com>
2017-04-24 23:10:26 -0500

    ya quedó primera mitad de la diagonal pa abajo

--- connect-4.lisp
+++ connect-4.lisp
@@ -126,6 +126,37 @@
     
     )
 (- puntos-dos puntos-uno))
+
+(defun traverse-diagonal-a (estado)
+    (setf ultima-ficha nil)
+    (setf unos 0)
+    (setf dos 0)
+    (setf puntos-uno 0)
+    (setf puntos-dos 0)
+    (setf TwoInRow 0 OneInRow 0)
+    (setf i 5)
+    (loop
+        (dotimes (n (+ i 1))
+            
+            (case (nth (- i n) (car (nth n estado)))
+                (0 (incf unos)(incf dos)(setf ultima-ficha 0))
+                (1 (incf unos)(setf dos 0)(setf ultima-ficha 1))
+                (2 (incf dos)(setf unos 0)(setf ultima-ficha 2)))
+ 
+                (if (eq ultima-ficha 2)(incf TwoInRow)(setf TwoInRow 0))
+                (if (eq ultima-ficha 1)(incf OneInRow)(setf OneInRow 0))
+                (if (eq TwoInRow 4)(incf puntos-dos 100))
+                (if (eq OneInRow 4)(incf puntos-uno 100))
+                (cond 
+                    ((and (> (- i n) 0) (eq 0 (nth (- i n) (car (nth n estado)))) (eq 0 (nth (- (- i n) 1) (car (nth n estado)))) (setf unos 0) (setf dos 0))))
+                (cond ((eq 4 unos) (incf puntos-uno) (decf unos)))
+                (cond ((eq 4 dos) (incf puntos-dos) (decf dos)))
+ 
+        )
+        (decf i)
+        (when (< i 1)(return))
+    )
+(- puntos-dos puntos-uno))
  
 (defun valor-nodo (estado)
 (+ (check4vertical estado) (check4horizontal estado)))
